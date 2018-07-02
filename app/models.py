fc6b4f75af21490cb2314b4ed7edf6e9f0eb3918
mzazakeith <mzazakeith@gmail.com>
2018-07-02 12:36:17 +0300

    user model, uri and migration done

--- app/models.py
+++ app/models.py
@@ -1,1 +1,37 @@
-# File for models/classes
+from datetime import datetime
+
+from flask import url_for, app, config
+from werkzeug.utils import redirect
+
+from . import db
+from flask_login import UserMixin
+from . import login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, index=True, nullable=False)
+    role = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
