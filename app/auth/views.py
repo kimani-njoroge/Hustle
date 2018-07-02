b63df9b6d2c5f069acdbb760493116e9e2d2645a
kimani-njoroge <kimani064@gmail.com>
2018-07-02 11:40:30 +0300

    create logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,6 +21,12 @@ def login():
     return render_template('auth/login.html', login_form=login_form, title=title)
 
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
 
 @auth.route('/register', methods=["GET", "POST"])
 def register():
