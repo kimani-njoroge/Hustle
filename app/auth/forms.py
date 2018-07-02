069ac5eb9c78fc581f5f74a945b310973eee3aca
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-07-02 13:29:45 +0300

    registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,19 @@
-from flask import Blueprint
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError,SelectField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
 
-auth = Blueprint('auth', __name__)
-from . import views, forms
+
+
+
+class RegistrationForm(FlaskForm):
+    '''
+    Function to create a wtf form for registering
+    '''
+    email = StringField('Your Email Address', validators=[Required(),Email()])
+    username = StringField('Enter your username', validators=[Required()])
+    role = SelectField('Role', choices = [('client', 'Client'), ('freelancer', 'Freelancer')])
+    password = PasswordField('Password', validators=[Required(), EqualTo('password_confirm', message="Passwords must match")])
+    password_confirm = PasswordField('Confirm Passwords', validators=[Required()])
+
+    submit = SubmitField('Sign Up')
