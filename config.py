e790f6a292161fe7f60e198348f4baa4c49c14d9
neville <kilonzoneville@gmail.com>
2018-07-03 18:23:51 +0300

    styling sign up

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:  @localhost/hustle'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://neville:quelde@localhost/hustle'
 
 class ProdConfig(Config):
     '''
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sam/PycharmProjects/Hustle/virtual"
+set -gx VIRTUAL_ENV "/home/neville/Desktop/Hustle/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
