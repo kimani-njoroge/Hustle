89c9d9908186ec7d3b2b2c6ac61ecb59877469a1
kimani-njoroge <kimani064@gmail.com>
2018-07-05 23:57:04 +0300

    redeploy to heroku

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 export SECRET_KEY="moringa12"
-export DATABASE_URL='postgres://ouyaffxarcrttn:954d3a1e6886457d7ab44e0385950131077f123eb527ea67dcb1f37b56e1e5ec@ec2-50-19-105-188.compute-1.amazonaws.com:5432/d3uv5mbdu682a'
+export DATABASE_URL='postgres://ismjbvjknlmqit:c50ef7393dc91ce91fd367fd53315e87b5d98e882ca2be921da53c105b5a2d2f@ec2-54-235-66-81.compute-1.amazonaws.com:5432/dd5mfg343rhl9d'
+
 
 
 
