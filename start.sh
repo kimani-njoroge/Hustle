678514a200cb068be830269c1c5769119e31f089
mzazakeith <mzazakeith@gmail.com>
2018-07-05 22:58:45 +0300

    deploying

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export SECRET_KEY="moringa12"
-export DATABASE_URL='postgres://sknpcuciqxjcph:7342bf0cc6ad54592f4f8c5cc62d3903a1a7151de171f914153fef7f9b04f667@ec2-54-235-66-81.compute-1.amazonaws.com:5432/d2b6cvls0amoth'
+export DATABASE_URL='postgres://zpndeqmdejoqll:c799a1fb81eaa49b5f5a0a120802027ab65e9e1956f272476464858ea1997dda@ec2-54-243-61-173.compute-1.amazonaws.com:5432/dfi1fvvkbso40n'
 
 python manage.py server
