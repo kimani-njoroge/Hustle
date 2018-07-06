2c5ddca41534b5796cf2f88af5c12dc5e3c38df7
kimani-njoroge <kimani064@gmail.com>
2018-07-06 08:57:21 +0300

    redeploying to heroku

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export SECRET_KEY="moringa12"
-export DATABASE_URL='postgres://ohtczhixlrlvuk:cfcb0f7dfec9d1ee4b06bbecd48b2a8151bb66eaccb3ee769f5991df5e3a1d24@ec2-107-22-241-243.compute-1.amazonaws.com:5432/d8kolhlbs6rui4'
-
+export DATABASE_URL='postgres://iefzokdlddgerj:5aa3ebd8cfd9a80db70e463a68147cba678aa5c00790d72a52c7bcae3adf9783@ec2-54-163-228-190.compute-1.amazonaws.com:5432/dbsfnqpskj9a8q'
 
 python manage.py server
